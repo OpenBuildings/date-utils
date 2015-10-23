83e34e9de33c2b26ebfa2778e12bfb77415938a3
Ivan Kerin <ikerin@gmail.com>
2015-10-23 11:28:31 +0300

    Fix php test in 5.4

--- tests/src/WeekDaysTest.php
+++ tests/src/WeekDaysTest.php
@@ -17,7 +17,7 @@ class WeekDaysTest extends PHPUnit_Framework_TestCase
             [5, new DateTime('2015-02-02'), new DateTime('2015-02-09')],
             [0, null, new DateTime('now')],
             [12, new DateTime('2015-03-01'), new DateTime('2015-03-17')],
-            [40, new DateTime('2015-03-01'), new DateTime('2015-03-01 + 40 weekdays')],
+            [4, new DateTime('2015-03-01'), new DateTime('2015-03-01 + 4 weekdays')],
             [5, new DateTime('2015-06-20'), new DateTime('2015-06-26')],
         ];
     }
